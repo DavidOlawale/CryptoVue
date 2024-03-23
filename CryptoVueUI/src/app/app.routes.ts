c334cbf668241e5f3ced073bd98ec6a6250e35f8
DavidOlawale <olawaledavid11@gmail.com>
2024-03-24 00:15:07 +0100

    Add components and configure routing

--- CryptoVueUI/src/app/app.routes.ts
+++ CryptoVueUI/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { InfoPageComponent } from './info-page/info-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UpdatePageComponent } from './update-page/update-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: InfoPageComponent,
+        title: 'BLP token data'
+    },
+    {
+        path: 'login',
+        component: LoginPageComponent,
+        title: 'Login'
+    },
+    {
+        path: 'update',
+        component: UpdatePageComponent,
+        title: 'Update BLP token data'
+    }
+];
