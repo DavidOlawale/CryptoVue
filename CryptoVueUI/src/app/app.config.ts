896086cade2796e1006e99a00bccd8d335ef6669
Olawale David <olawaledavid11@gmail.com>
2025-03-03 05:52:34 -0800

    Make progress on tokenomics frotend Implementation

--- CryptoVueUI/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr } from 'ngx-toastr';
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideToastr()]
-};
